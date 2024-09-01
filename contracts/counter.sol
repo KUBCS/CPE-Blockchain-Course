fc74aabcd44446babb6b061319dc5d93c17b098e
Nawapat Buakoet <nawapat.bu@ku.th>
2024-09-01 21:09:56 +0700

    Create counter.sol
    Signed-off-by: Nawapat Buakoet <nawapat.bu@ku.th>

---
+++ contracts/counter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Counter {
+    // State variable to store the count
+    //? TODO add the variable for the count.
+    // ...
+
+    // Function to increment the count
+    function increment() public {
+       //? TODO add the code for the increment function.
+       // ...
+    }
+
+    // Function to decrement the count
+    function decrement() public {
+        //? TODO add the code for decrement
+        // ...
+    }
+
+    // Function to get the current count
+    function getCount() public view returns (...) {
+       //? TODO add the code for return the value of the count variable.
+       // ...
+    }
+}
