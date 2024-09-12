90ce448bff967e6f43cc1cf7ba26e1fff36f615d
Nawapat Buakoet <nawapat.bu@ku.th>
2024-09-12 22:41:40 +0700

    Create One.sol
    Signed-off-by: Nawapat Buakoet <nawapat.bu@ku.th>

---
+++ contracts/practices/One.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.27;
+
+contract One {
+    string public str;
+    string[] public strings;
+
+    uint256 public num;
+
+    address public addr;
+
+    bytes public b;
+
+    bool public boolean;
+}
