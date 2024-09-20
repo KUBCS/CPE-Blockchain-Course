98b6ba05bbc61a780ccc899889f1bf66be4d513e
Nawapat Buakoet <nawapat.bu@ku.th>
2024-09-20 14:15:31 +0700

    Update Four.sol
    Signed-off-by: Nawapat Buakoet <nawapat.bu@ku.th>

--- contracts/practices/Four.sol
+++ contracts/practices/Four.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
 
 contract ERC20Token is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
-    constructor(
-        address initialOwner
-    ) ERC20("Nawapat", "BEAM") Ownable(initialOwner) {}
+    constructor() ERC20("BEAM", "beam") Ownable() {}
 
     function pause() public onlyOwner {
         _pause();
@@ -24,13 +21,11 @@ contract ERC20Token is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
         _mint(to, amount);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal override(ERC20, ERC20Pausable) {
-        super._update(from, to, value);
+     function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Pausable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
